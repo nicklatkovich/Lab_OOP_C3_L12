23a334182d0150831371d02a652558115de09237
NickLatkovich <nicklatkovich@gmail.com>
2016-12-20 15:51:52 +0300

    Virtual Window

--- OOPC3L12/Source.cpp
+++ OOPC3L12/Source.cpp
@@ -3,10 +3,36 @@
 class CMainWnd : public CFrameWnd {
 public:
 	CMainWnd();
+	afx_msg void OnPaint();
+	CDC m_memDC;
+	CBitmap m_bmp;
+	CBrush m_bkbrush;
+private:
+	DECLARE_MESSAGE_MAP();
 };
+
+BEGIN_MESSAGE_MAP(CMainWnd, CFrameWnd)
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
 
 CMainWnd::CMainWnd() {
 	Create(NULL, L"Lab12", WS_OVERLAPPEDWINDOW, rectDefault, NULL, NULL);
+	int maxX = GetSystemMetrics(SM_CXSCREEN);
+	int maxY = GetSystemMetrics(SM_CYSCREEN);
+	CClientDC dc(this);
+	m_memDC.CreateCompatibleDC(&dc);
+	m_bmp.CreateCompatibleBitmap(&dc, maxX, maxY);
+	m_memDC.SelectObject(&m_bmp);
+	m_bkbrush.CreateStockObject(WHITE_BRUSH);
+	m_memDC.SelectObject(&m_bkbrush);
+	m_memDC.PatBlt(0, 0, maxX, maxY, PATCOPY);
+}
+
+void CMainWnd::OnPaint() {
+	CPaintDC paintDC(this);
+	RECT clientRect;
+	this->GetClientRect(&clientRect);
+	paintDC.BitBlt(0, 0, clientRect.right, clientRect.bottom, &m_memDC, 0, 0, SRCCOPY);
 }
 
 class CMyApp : public CWinApp {
